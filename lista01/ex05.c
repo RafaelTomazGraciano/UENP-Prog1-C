9f1f8e939f9990d320d7fc152c06954dd15234b2
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-04-20 15:00:06 -0300

    ex05

---
+++ lista01/ex05.c
@@ -0,0 +1,22 @@
+/*Faca um programa que receba as coordenadas de dois pontos (A e B) no plano cartesiano,
+calcule e mostre distancia entre esses dois pontos. Para calcular da distancia (d) entre dois
+pontos (A e B), temos:*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float d, xa, xb, ya, yb;
+    printf("Digite a coordenada x do ponto a :");
+    scanf("%f",&xa);
+    printf("Digite a coordenda x do ponto b: ");
+    scanf("%f",&xb);
+    printf("Digite a coordenada y no ponto a: ");
+    scanf("%f",&ya);
+    printf("Digite a coordenada y no ponto b: ");
+    scanf("%f",&yb);
+    d=sqrt(pow((xb-xa),2)+pow((yb-ya),2));
+    printf("A distancia entre A e B e: %.2f",d);
+    return 0;
+}
