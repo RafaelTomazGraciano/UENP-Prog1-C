b57fd13fa76531dfce8d8ba7ff3fd632ca25d208
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-04-28 12:49:46 -0300

    ex01

---
+++ Lista 02/ex01.c
@@ -0,0 +1,19 @@
+/*Implemente um programa que leia as variaveis a, b e c e imprima os valores das quatros
+formulas seguintes:
+a) (a ∗ b)/c
+b) a²+b+5c
+c) a*b*c+b+c/3*5-1
+d) (a*b*c)³/2 */
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c;
+    printf("Digite o valor de a: ");
+    printf("Digite o valor de b: ");
+    printf("Digite o valor de c: ");
+    scanf("%f%f%f",&a, &b, &c);
+    return 0;
+}
