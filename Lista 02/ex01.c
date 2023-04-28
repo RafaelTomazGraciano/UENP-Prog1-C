160b90f8fc41d56e75389f7a7a4588c266453df1
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-04-28 13:31:53 -0300

    ex01

--- Lista 02/ex01.c
+++ Lista 02/ex01.c
@@ -1,8 +1,8 @@
 /*Implemente um programa que leia as variaveis a, b e c e imprima os valores das quatros
 formulas seguintes:
 a) (a ∗ b)/c
-b) a²+b+5c
-c) a*b*c+b+c/3*5-1
+b) a²+b+5*c
+c) (a*b*c+b)+(c/3)*5-1
 d) (a*b*c)³/2 */
 
 #include <stdio.h>
@@ -12,8 +12,26 @@ int main()
 {
     float a, b, c;
     printf("Digite o valor de a: ");
-    printf("Digite o valor de b: ");
-    printf("Digite o valor de c: ");
-    scanf("%f%f%f",&a, &b, &c);
+    scanf("%f", &a);
+    printf("\nDigite o valor de b: ");
+    scanf("%f", &b);
+    printf("\nDigite o valor de c: ");
+    scanf("%f", &c);
+
+    // Formula letra a
+
+    printf("\n O resultado da formula a e : %.2f", (a*b)/c);
+
+    // Formula letra b
+
+    printf("\n O resultado da formula b e: %.2f", a*a+b+5*c);
+
+    // Formula letra c
+
+    printf("\n O resultado da formula c e: %.2f", (a*b*c+b)+(c/3)*5-1);
+
+    // Formula letra d
+
+    printf("\n O resultado da formula d e: %.2f", pow ((a*b*c),3)/2);
     return 0;
 }
