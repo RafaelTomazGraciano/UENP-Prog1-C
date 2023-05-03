b33d231388a852dd3d3a5be8bf9f13d215f25240
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-05-03 15:26:52 -0300

    ex01

---
+++ Programas sala de aula/ex07.c
@@ -0,0 +1,25 @@
+/*calculadora simples*/
+
+#include <stdio.h>
+
+int main()
+{
+    float x, y;
+    char operacao;
+    printf("Digite um numero, um operador e outro numero: ");
+    scanf("%f %c %f", &x, &operacao, &y);
+    //avalia op e executa comando correspondente
+    switch (operacao) {
+        case '+':
+        printf("Adicao = %.2f \n", x+y); break;
+        case '-':
+        printf("Subtracao = %.2f \n", x-y); break;
+        case '*':
+        printf("Multiplicacao = %.2f \n", x*y ); break;
+        case '/':
+        printf("Divisao = %.2f", x/y); break;
+        default:
+        printf("Operador desconhecido");
+    }
+    return 0;
+}
