d4e5ba1ccd1ee83481251dff7a8c745ccf280d94
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-05-06 16:18:31 -0300

    ex05

---
+++ Lista 03/ex06.c
@@ -0,0 +1,18 @@
+/*Uma pequena mercearia resolveu dar descontos para agradar seus clientes. A mercearia
+contem apenas 4 itens, cujos precos sao dados pela Tabela 1.
+A regra de desconto e bem simples: se a quantidade de produtos comprados for igual ou
+maior que  15 itens, entao o desconto e concedido. O desconto tambem e concedido caso o
+valor total da compra seja maior ou igual R$ 40,00. O valor do desconto e de 15%.
+Faca uma programa que leia o codigo do produto, a quantidade comprada e exiba o valor
+que o cliente deve pagar, ja considerando o desconto quando aplicavel. Considere que o
+cliente so pode comprar um unico tipo de produto cada vez que usar o seu software.
+                      Tabela 1: Precos dos produtos
+                Codigo             Preco unitario
+                   1               R$ 5,30
+                   2               R$ 6,00
+                   3               R$ 3,20
+                   4               R$ 2,50*/
+
+#include <stdio.h>
+
+int main()
