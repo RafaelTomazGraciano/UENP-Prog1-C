3e880b87c8b63720fdda2a6efd0b5330f5526b44
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-05-06 13:58:06 -0300

    ex01

---
+++ Lista 03/ex01.c
@@ -0,0 +1,19 @@
+/*Faça um programa que receba como entrada um caractere e informe se ele ́e uma vogal ou
+não.*/
+
+#include <stdio.h>
+
+int main()
+{
+    char letra;
+    printf("Digite uma letra: ");
+    scanf("%c", &letra);
+    letra = tolower(letra);
+    if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
+        printf("\n Este caractere e uma vogal"); 
+    }
+    else{
+        printf("\n Este caractere e uma consoante");
+    }
+    return 0;
+}
