336a52852381f3a89ed96268ad4601d055decb56
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-07-12 14:17:05 -0300

    ex

--- Lista 05/ex02.c
+++ Lista 05/ex02.c
@@ -3,12 +3,27 @@ ou nao.*/
 
 #include <stdio.h>
 
-int primo(){
-
+int primo(int numero){
+  int primo = 0;
+  for(int contador = 1; contador <= numero; contador++){
+    if(numero % contador == 0){
+      primo ++;
+    }
+  }
+  return (primo == 2)? 1 : 0;
 }
 
 int main()
 { 
-
+  int numero;
+  printf("Digite um numero: ");
+  scanf("%d",&numero);
+  switch(primo(numero)){
+    case 1:
+    printf("O numero e primo");
+    break;
+    case 0:
+    printf("O numero nao e primo");
+  }
   return 0;
 }
