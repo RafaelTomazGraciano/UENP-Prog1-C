8b2895817aeaffc0c2c5638e6e720ef12c57c961
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-08-09 14:52:34 -0300

    ex

--- Lista 05/ex03.c
+++ Lista 05/ex03.c
@@ -4,7 +4,29 @@ Imprima os dois vetores resultante.*/
 
 #include <stdio.h>
 
+#define TAM 50
+
 int main(){
-  
+  int vetor[TAM], vetorpar[TAM], vetorimpar[TAM], k = 0, l = 0;
+  for(int i = 0; i < TAM; i++){
+    printf("Digite o numero da posicao %d: ", i);
+    scanf("%d", &vetor[i]);
+    if(vetor[i] % 2 == 0){
+      vetorpar[k] = vetor[i];
+      k++;
+    }
+    else{
+      vetorimpar[l] = vetor[i];
+      l++;
+    }
+  }
+    printf("\nNumeros pares");
+  for(int i = 0; i < k; i++){
+    printf("\n%d", vetorpar[i]);
+  }
+  printf("\nNumeros impares");
+  for(int i = 0; i < k; i++){
+    printf("\n%d", vetorimpar[i]);
+  }
   return 0;
 }
---
+++ Lista 05/ex04.c
@@ -0,0 +1,19 @@
+/*Faca um programa que leia uma sequencia de 21 numeros inteiros e armazene em um vetor.
+Depois, determine se o ultimo numero lido esta presente nos 20 primeiros e mostre as
+posicoes do vetor em que ele se encontra.*/
+
+#include <stdio.h>
+
+int main(){
+  int vetor[21];
+  for(int i = 0; i < 21; i++){
+    printf("Digite o numero da posicao %d: ", i);
+    scanf("%d", &vetor[i]);
+  }
+  for(int i = 0; i < 21; i++){
+    if(vetor[20] == vetor[i]){
+      
+    }
+  }
+return 0;
+}
