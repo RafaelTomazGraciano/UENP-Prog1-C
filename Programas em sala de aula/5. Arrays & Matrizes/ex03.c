e74baf29f390c5c265681c3875f3d5bcaae0c281
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-08-16 11:23:28 -0300

    ex

--- Programas em sala de aula/5. Arrays & Matrizes/ex03.c
+++ Programas em sala de aula/5. Arrays & Matrizes/ex03.c
@@ -1,9 +1,11 @@
-/*Usuario define a matriz, coloca as notas, e mostra, e depois mostra o valores multiplicados por 5*/
+/*Usuario define a matriz, coloca as notas, e depois mostre os 
+valores multiplicados por 5, e tambem as medias das notas*/
 
 #include <stdio.h>
 
 int main(){
   int lin, col;
+  float soma = 0, media = 0;
   printf("Digite o tamanho da matriz. Linhas e colunas\n");
   scanf("%d%d", &lin, &col);
   float notas[lin][col];
@@ -26,6 +28,16 @@ int main(){
       printf(" %.2f", notas[i][j]*5);
     }
     printf("\n");
+  }
+  printf("\nMedia Notas\n");
+  for(int i = 0; i <lin; i++){
+    soma = 0;
+    for(int j = 0; j < col; j++){
+      soma += notas[i][j];
+      media = soma/(j+1);
+    }
+    printf("\n");
+    printf(" %.2f", media);
   }
   return 0;
 }
