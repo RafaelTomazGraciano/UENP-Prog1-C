ceb3b0bdc35a9298005a8678dabbef32ac11415c
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-09-27 10:57:49 -0300

    ex

--- Programas em sala de aula/String/ex01.c
+++ Programas em sala de aula/String/ex01.c
@@ -1,1 +1,19 @@
-/**/
+/*leitura padrao com fgets e gets*/
+
+# include <stdio.h>
+
+int main(){
+
+char frase[16];
+
+printf("Digite uma frase: ");
+fgets(frase, 16, stdin); // usando fgets
+setbuf(stdin, NULL);
+printf("%s\n", frase);
+
+printf("Digite uma frase novamente: ");
+gets(frase); // usando gets
+
+printf("%s\n", frase);
+return 0;
+}
