2d03b54c38aeb42ee03af5334d9f1a93223a1415
Rafael Tomaz Graciano <124164062+RafaelTomazGraciano@users.noreply.github.com>
2023-11-28 19:33:29 -0300

    configuring

---
+++ Lista 09/ex09.c
@@ -0,0 +1,24 @@
+/*Faca um programa para gerenciar o cadastro de um numero indeterminado de games/jogos
+digitais. Para isso:
+a. Defina uma estrutura adequada para representar cada jogo, incluindo os campos: nome
+do jogo, genero (por ex., RPG, acao, simulacao,etc.), modo de jogo (single player,
+multiplayer), plataforma suportada e posicao no ranking. Outros campos podem ser
+incluıdos, caso julgue pertinente.
+b. Crie uma funcao para ler as informacoes dos jogos e armazenar em um vetor de
+estrutura (array de struct).
+c. Crie uma funcao para exibir todos os jogos cadastrados.
+d. Crie uma funcao que receba o genero do jogo e mostre as informacoes de todos os
+jogos que se encaixam nessa classificacao.
+e. Crie uma funcao que leia do usuario um numero entre 1 e 5 (quanto maior o ranking,
+maior a classificacao). Em seguida, exiba as informacoes do jogo correspondendo
+a posicao do ranking dos jogos de que voce mais gostou, de acordo com o que foi
+informado pelo usuario.
+f. Crie uma funcao que receba o nome do jogo e indique se ele esta entre os seus favoritos
+ou nao.*/
+
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
